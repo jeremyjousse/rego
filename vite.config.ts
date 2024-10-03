8f2188f79544ca6d0b20df633bb86451a2563d9d
Jérémy Jousse <jeremy.jousse@decathlon.com>
2024-10-03 18:33:04 +0200

    fix: github build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rego/",
   plugins: [react()],
   build: {
     outDir: "./build",
